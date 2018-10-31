a59d41f98c5df5d4efdf62af194c026424b4bbb7
estherpato <estherpato@gmail.com>
2018-10-31 22:29:08 +0100

    Add classes on each component

---
+++ src/components/PhoneListContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import './stylesheets/App.css';
+
+class PhoneListContainer extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default PhoneListContainer;
